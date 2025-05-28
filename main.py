b26911657f1dea947101511fe2f9cbcb285e8984
Miracle <137496016+im-Miracleous@users.noreply.github.com>
2025-05-28 22:36:42 +0700

    Inisiasi menu v1 + tambah resi

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # File : main.py 
 # Program menampilkan interface utama untuk aplikasi booking tiket bioskop
 
+import jadwalFilm
+import tempatduduk
+import resi
+
 def menu():
     print("Selamat datang di bioskop Cinegaje!")
     print('"Walau nonton tak jelas, abis nonton pasti puas!"')
@@ -8,12 +12,21 @@ def menu():
     print("Silahkan ketikkan angka sesuai menu yang ingin dipilih:")
     print("1. Lihat Jadwal Film")
     print("2. Booking Tiket")
-    print("3. Histori Pesanan")
+    print("3. Print Resi")
     print("4. Keluar")
     print("--------------------------------------")
     choose = int(input("Pilihan: "))
-    print()
-    return choose
+    while (choose != 4):
+        if (choose == 1):
+            jadwalFilm.lihat_jadwal_film()
+        elif (choose == 2):
+            tempatduduk.main()
+        elif (choose == 3):
+            resi.main()
+        elif (choose == 4):
+            print("Keluar dari program...")
+        choose = int(input("Pilihan: "))
+    return
 
 def main():
     menu()
